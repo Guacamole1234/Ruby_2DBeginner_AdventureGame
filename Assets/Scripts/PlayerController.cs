47f3b418705a01e11938f99af2ffa7d8b4e69f37
Miguel Domínguez Díaz <Thegamerlapalma@gmail.com>
2025-01-17 09:49:37 +0000

    17/01/25
    3.2.6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,39 +8,19 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0f;
-        float vertical = 0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1f;
-        }
-     else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = +1f;
-        }
-     Debug.Log(horizontal);
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            horizontal = +1f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            horizontal = -1f;
-        }
-
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.01f * horizontal;
-        position.y = position.y + 0.01f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 6f * Time.deltaTime;
         transform.position = position;
     }
 }
